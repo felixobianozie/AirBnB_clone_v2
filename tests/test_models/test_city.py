a54ccaf7c7ecc0f1c2e9e908cf15c205208b96f3
felixobianozie <felixobianozie@gmail.com>
2022-09-20 23:18:33 +0000

    More test cases for city model

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""Unittest for the CityModel"""
+"""Unittest for CityModel class"""
 
 import unittest
+from models.city import City
 
 
-class TestCityModel(unittest.TestCase):
-    """Test cases for the City Model"""
+class test_City(unittest.TestCase):
+    """Test cases for class CityModel"""
 
-    def test_city(self):
+    def setUp(self):
+        """Basic setup parameters"""
+        self.value = City
+        self.name = "City"
+        self.attr_list = ["state_id", "name"]
+
+    def tearDown(self):
+        """Basic teardown functions"""
         pass
 
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_attributes(self):
+        """Test City attributes"""
+        new = self.value()
+        self.assertTrue(hasattr(new, "state_id"))
+        self.assertTrue(hasattr(new, "name"))
+        self.assertTrue(type(new.state_id),str)
+        self.assertTrue(type(new.name),str)
 
